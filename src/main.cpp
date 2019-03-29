d0f43515db1c49df18bf4d711bd448b149fa1ba2
KD Technology <34989581+yuvalkedar@users.noreply.github.com>
2019-03-29 10:30:31 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,111 @@
-/*
-Direct Drive LED Matrix
-
-Author: Yuval Kedar, KD Tech.
-Platform: Arduino Pro-mini
-Date: Jan 2019
-*/
-
 #include <Arduino.h>
-#include "fonts.h"
+#include "font_5x7.h"
 
-//rows are negative
-//columns are positive
-#define WIDTH (5)
-#define HEIGHT (8)
+#define MATRIX_WIDTH (5)
+#define MATRIX_HEIGHT (8)
+#define MAX_MATRIX_WIDTH (MATRIX_WIDTH - 1)
 
-#define COL_0  (9)
-#define COL_1  (8)
-#define COL_2  (7)
-#define COL_3  (6)
-#define COL_4  (5)
-#define ROW_0  (10)
-#define ROW_1  (11)
-#define ROW_2  (12)
-#define ROW_3  (13)
-#define ROW_4  (14)
-#define ROW_5  (15)
-#define ROW_6  (16)
-#define ROW_7  (17)
+#define BITS_IN_BYTE (8)
+#define SERIAL_BAUDRATE     (115200)
 
-const uint8_t row[HEIGHT] = {ROW_0, ROW_1, ROW_2, ROW_3, ROW_4, ROW_5, ROW_6, ROW_7};
-const uint8_t col[WIDTH] = {COL_0, COL_1, COL_2, COL_3, COL_4};
+uint8_t matrix_buffer[MATRIX_WIDTH * MATRIX_HEIGHT / BITS_IN_BYTE] = {0};
 
-uint8_t pixel[HEIGHT][WIDTH];  //pixel[rows][columns]
+void set_vertical_bitmask(uint8_t bitmask) {
+    bitmask = ~bitmask;
+    PORTB = (PORTB & 0xC3) | (bitmask << 2) & 0x3C;
+    PORTC = (PORTC & 0xF0) | (bitmask >> 4) & 0x3F;
+}
 
-const char message[] = "BANANA";
+void set_horizental_index(uint8_t index) {
+    uint8_t bitmask = _BV(MAX_MATRIX_WIDTH - index);
+    PORTD = (PORTD & 0x1F) | (bitmask << 5) & 0xE0;
+    PORTB = (PORTB & 0xFC) | (bitmask >> 3) & 0x03;
+}
 
-//////////////////////FUNCTIONS///////////////////////
-
-void clear_screen(){
-  for(uint8_t r = 0; r < HEIGHT; r++){
-    for(uint8_t c = 0; c < WIDTH; c++){ //LOL c++
-      pixel[r][c] = 0;
+void scan_matrix() {
+    for (uint8_t x = 0; x < MATRIX_WIDTH; x++) {
+        set_horizental_index(x);
+        set_vertical_bitmask(matrix_buffer[x]);
+        delay(1);
+        set_vertical_bitmask(0);
     }
-  }
 }
 
-void set_pixel(uint8_t x, uint8_t y, bool state){ pixel[x][y] = state;}
+void delay_with_scan(uint32_t ms) {
+    ms += millis();
+    while(ms > millis())
+        scan_matrix();    
+}
 
-void draw_pixel(){
-  for(uint8_t r = 0; r < HEIGHT; r++){
-    for(uint8_t i = 0; i < WIDTH; i++) digitalWrite(col[i], LOW); // turn previous col off
-    digitalWrite(row[r], LOW);
-    for(uint8_t c = 0; c < WIDTH; c++){ //LOL c++
-      digitalWrite(col[c], pixel[r][c]);  // get the state of the current pixel
-      delayMicroseconds(10);
+void clear_matrix() {
+    memset(matrix_buffer, 0, sizeof(matrix_buffer));
+}
+
+void draw_vertical_bits(uint8_t x, uint8_t bitmask) {
+    if (x > MAX_MATRIX_WIDTH)
+        return;
+    matrix_buffer[x] = bitmask;
+}
+
+void shift_matrix_left(uint8_t new_bitmask) {
+    for (uint8_t x = 1; x < MATRIX_WIDTH; x++)
+        matrix_buffer[x - 1] = matrix_buffer[x];
+    matrix_buffer[MAX_MATRIX_WIDTH] = new_bitmask;
+}
+
+void print_char(uint8_t c, uint8_t x) {
+	if ((c < FONT_FIRST_CHAR) || (c > (FONT_FIRST_CHAR + FONT_CHARS_COUNT)))
+        return;
+
+	uint16_t font_index = (c - FONT_FIRST_CHAR) * FONT_WIDTH;
+
+	for (uint8_t i = 0; i < FONT_WIDTH; i++)
+		draw_vertical_bits(x + i, FONT_READ_DATA(font_index + i));
+}
+
+uint8_t get_str_width(char *str) {
+    return strlen(str) * FONT_WIDTH;
+}
+
+void scroll_str(char *str, uint32_t speed_ms) {
+	// uint8_t str_w = get_str_width(str);
+    char c;
+
+    while(c = *str++){
+        for (uint8_t x = 0; x < MATRIX_WIDTH; x++) {
+            shift_matrix_left(0);
+            print_char(c, MAX_MATRIX_WIDTH - x);
+            delay_with_scan(speed_ms);
+        }
+        shift_matrix_left(0);
+        delay_with_scan(speed_ms);
+        shift_matrix_left(0);
+        delay_with_scan(speed_ms);
     }
-    digitalWrite(row[r], HIGH);
-  }
-}
-
-void set_char(uint8_t pattern){
-  for(uint8_t r = 0; r < HEIGHT; r++){
-    for(uint8_t c = 0; c < WIDTH; c++){ //LOL c++
-      pixel[r][c] = patterns[pattern][r][c];
+    for (uint8_t x = 0; x < 3; x++) {
+        shift_matrix_left(0);
+        delay_with_scan(speed_ms);
     }
-  }
 }
 
-void print_text(char *c){
-  
+void setup() {
+	Serial.begin(SERIAL_BAUDRATE);
+	Serial.println(F("\nstart!"));
+
+    /* set pins 5-7 as OUTPUTs */
+    DDRD |= 0xE0;
+
+    /* set pins 8-13 as OUTPUTs */
+    DDRB |= 0x3F;
+
+    /* set pins A0-A3 as OUTPUTs */
+    DDRC |= 0x0F;
+
+    scan_matrix();
+    // print_char('a', 0);
 }
 
-void setup(){
-  for(uint8_t r = 0; r < HEIGHT; r++){
-    pinMode(row[r], OUTPUT);
-    digitalWrite(row[r], HIGH); //turn all rows (negative) off.
-  }
-
-  for(uint8_t c = 0; c < WIDTH; c++){ //LOL c++
-    pinMode(col[c], OUTPUT);
-    digitalWrite(col[c], LOW); //turn all columns (positive) off.
-  }
-
-  clear_screen();
-  set_pattern(pattern);
-}
-
-void loop(){
-//  set_pixel(0, 0, 1);
-//  set_pixel(1, 1, 1);
-//  set_pixel(2, 2, 1);
-//  set_pixel(3, 3, 1);
-//  draw_pixel();
-
-//pattern = ++pattern % numPatterns;
-//draw_attern(pattern, 60);
-draw_pixel();
+void loop() {
+    delay_with_scan(100);
+    scroll_str("banana", 200);
 }
